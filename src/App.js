72913e6e09f5ddb82729d21ebb236d0691dd3d05
Deepak Porwal <deepak.porwal@impelsys.com>
2023-10-22 14:10:40 +0530

    Save data to firebase.   For more detail check realtime database on https://console.firebase.google.com/

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
-import { UseSelector, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
+let initialLoad = true;
 
 function App() {
   const showCart = useSelector(state => state.ui.isVisible);
+  const cartData = useSelector(state => state.cart);
+  useEffect(() =>
+    {
+      const cartRequest = async() => {
+        const response = await fetch('https://redux-cart-6f83e-default-rtdb.firebaseio.com/cart.json', {
+          method: 'PUT',
+          body: JSON.stringify(cartData)
+        });
+        if(!response.ok) {
+          throw new Error("Could not send request");
+        }
+      }
+    if(initialLoad) { // Stop sending request first time when page load
+      initialLoad = false;
+      return;
+    }
+      cartRequest().catch(error => {
+        console.log(error);
+      });
+  }, [cartData]);
+
   return (
     <Layout>
       {showCart && <Cart />}
